9268872e094e9e1ed067e2edbd978315fbbfce58
Max Wank <maxwank11@gmail.com>
2022-11-04 14:20:30 -0400

    finished

--- src/selectionSort.java
+++ src/selectionSort.java
@@ -1,19 +1,27 @@
 public class selectionSort {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] elements = {1, 5, 3, 86, 256, 421, 9, 510, 51, 24, 60};
         clear();//clear
-        System.out.println("Entered Values");
-        for (int i = 0; i <= 10; i ++){                      //print the array
-            System.out.print(elements[i]+"|");
+        System.out.println("Initial Placement");
+        for (int i = 0; i <= 10; i++) {                      //print the array
+            System.out.print(elements[i] + "|");
         }
         System.out.println();                                //move cursor to next line
-        for (int bottom = 0; bottom <= 9; bottom ++){
-            int min= bottom;                                //assume first value is smallest
-            for (int current=bottom + 1; bottom <=10; bottom ++){
-                if (elements[current]<elements[min]){       // check if the smallest value is greater than the current min
-                    min=current;
+        for (int bottom = 0; bottom <= 9; bottom ++) {
+            int min = bottom;                                //assume first value is smallest
+            for (int current = bottom + 1; current <= 10; current ++) {
+                if (elements[current] < elements[min]) {       // check if the smallest value is greater than the current min
+                    min = current;
                 }
             }
+            //swap function
+            int temp = elements[min];
+            elements[min] = elements[bottom];
+            elements[bottom] = temp;
+        }
+        System.out.println("Final Placement");
+        for (int i = 0; i <= 10; i++) {                      //print the array
+            System.out.print(elements[i] + "|");
         }
     }
 
